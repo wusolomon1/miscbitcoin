08a425fcdc8a6d6dae12856a9c5770b1ab9490c9
solomon wu <wusolomon1@gmail.com>
2015-09-15 12:45:54 +0800

    change gen hdkey to take parameters of how many to generate.

--- genbitcoinaddr/genhdkey.js
+++ genbitcoinaddr/genhdkey.js
@@ -4,10 +4,23 @@
 var bitcore = require('bitcore');
 var HDPrivateKey = bitcore.HDPrivateKey;
 
+// take input
+if (process.argv.length < 3) {
+	console.log("usage: node genhdpk <num_to_gen>")
+	process.exit(0);
+}
+
+var numKey = parseInt(process.argv[2]);
+
 //var hdPrivateKey = new HDPrivateKey();
 //var seed = hdPrivateKey.xprivkey;
+// test seed
 var seed = "xprv9s21ZrQH143K2YuHXNmscJpws3crfHYJYMwMUZ3tyWuY8ToRNxq6YiVxhHxWtE8Rboy1xGr7R8MLBUrsh1QKhpxM29X5GrjbQbzQ1Gvz441";
-console.log("seed: " + seed);
-var retrieved = new HDPrivateKey(seed);
-var derived = retrieved.derive("m/1/2/150/1000000000");
-console.log(derived);
+//var seed = "077961a06851ad8d4ad9315c601346bc";
+//console.log("seed: " + seed);
+for (var i=0; i<numKey; i++) {
+	var retrieved = new HDPrivateKey(seed);
+	var derived = retrieved.derive("m/"+i);
+	var wif = derived.privateKey.toWIF();
+	console.log(wif);
+}

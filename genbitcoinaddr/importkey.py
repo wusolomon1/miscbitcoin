0903c3bd22bca85cb556d986fc6b07776a6665d9
solomon wu <wusolomon1@gmail.com>
2015-09-15 13:21:37 +0800

    importkey takes parameter

--- genbitcoinaddr/importkey.py
+++ genbitcoinaddr/importkey.py
@@ -2,12 +2,19 @@
 simple script to import private key into coin daemon
 '''
 import os
+import sys
 
-cli = 'bitcoin-cli'
+cli = 'bitcoin-cli -gcoin'
 
-with open('test2.log') as fp:
+if len(sys.argv) < 2:
+	print 'usage: python importkey.py <filename>'
+
+filename = sys.argv[1]
+
+with open(filename) as fp:
     for line in fp:
-        cmd = cli + ' importprivkey ' + str(line)
+    	# add 0 at the end so that it does not rescan blockchain each time.
+        cmd = cli + ' importprivkey ' + str(line) + " 0"
         os.system(cmd)
         print cmd
         

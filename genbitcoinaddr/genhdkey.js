a81ba5233533990872e9f81f8eec1094ad89b1c6
solomon wu <wusolomon1@gmail.com>
2015-09-16 17:15:05 +0800

    genhdkey, add startnum and endnum

--- genbitcoinaddr/genhdkey.js
+++ genbitcoinaddr/genhdkey.js
@@ -6,23 +6,25 @@ var HDPrivateKey = bitcore.HDPrivateKey;
 
 // take input
 if (process.argv.length < 3) {
-	console.log("usage: node genhdpk <num_to_gen> [<seed>]")
+	console.log("usage: node genhdpk <startNum> <endNum> [<seed>]" 
+		+ "\nstartNum and endNum are inclusively generated."
+		+ "\neg. if you do  node genhdpk 1 1000 xprv932423423..."
+		+ "\nnext time, you should do  node genhdpk 1001 2000 xprv932423423...");
 	process.exit(0);
 }
 
-var numKey = parseInt(process.argv[2]);
+var startNum = parseInt(process.argv[2]);
+var endNum = parseInt(process.argv[3]);
 
 //var hdPrivateKey = new HDPrivateKey();
 //var seed = hdPrivateKey.xprivkey;
 // optionally get a seed.
 // test seed
 var seed = "xprv9s21ZrQH143K2YuHXNmscJpws3crfHYJYMwMUZ3tyWuY8ToRNxq6YiVxhHxWtE8Rboy1xGr7R8MLBUrsh1QKhpxM29X5GrjbQbzQ1Gvz441";
-if (process.argv.length >= 4) {
-	seed = process.argv[3];
+if (process.argv.length >= 5) {
+	seed = process.argv[4];
 }
-//var seed = "077961a06851ad8d4ad9315c601346bc";
-//console.log("seed: " + seed);
-for (var i=0; i<numKey; i++) {
+for (var i=startNum; i<=endNum; i++) {
 	var retrieved = new HDPrivateKey(seed);
 	var derived = retrieved.derive("m/"+i);
 	var wif = derived.privateKey.toWIF();
